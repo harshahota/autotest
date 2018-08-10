8c04605bc9fbdf8b2362dd8bf34888c228949157
sriharsha.hota <sriharsha.hota@mu-sigma.com>
2018-08-10 15:12:27 +0530

    dockerfile6

--- e2e/conf.js
+++ e2e/conf.js
@@ -1,18 +1,21 @@
 var Jasmine2HtmlReporter = require('protractor-jasmine2-html-reporter');
 
 exports.config = {
-    specs: ['./tests/*.js'],
-    onPrepare: function () {
-        browser.ignoreSynchronization = true;
-    },
-    capabilities: {
-      'browserName': 'firefox'
-    },
-    onPrepare: function() {
-        jasmine.getEnv().addReporter(
-          new Jasmine2HtmlReporter({
-            savePath: 'target/report'
-          })
-        );
-     }
+  specs: ['./tests/*.js'],
+  onPrepare: function () {
+    browser.ignoreSynchronization = true;
+  },
+  capabilities: {
+    'browserName': 'chrome',
+    'chromeOptions': {
+      'args': ['--headless']
+    }
+  },
+  onPrepare: function () {
+    jasmine.getEnv().addReporter(
+      new Jasmine2HtmlReporter({
+        savePath: 'target/report'
+      })
+    );
+  }
 }
