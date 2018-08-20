6fdbad555cf12b972cf07ff07e368c6a4ca8bcce
sriharsha.hota <sriharsha.hota@mu-sigma.com>
2018-08-20 20:28:28 +0530

    working version 5

--- pass.sh
+++ pass.sh
@@ -4,4 +4,5 @@ echo "......................................"
 echo ".........the pass message............"
 echo "......................................"
 echo "root=harshahota@gmail.com\nmailhub=smtp.gmail.com:587\nAuthUser=jenkinsupdates@gmail.com\nAuthPass=jenkinspassword\nUseTLS=YES\nUseSTARTTLS=YES\nFromLineOverride=YES" >> /etc/ssmtp/ssmtp.conf
+ls
 echo -e "to: receiver@domain.tld\nsubject: Pass Report\n"| (cat - && uuencode /report.zip report.zip) | ssmtp harshahota123@gmail.com
