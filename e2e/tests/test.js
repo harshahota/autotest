83b482dcd1d0d81fb8da0c611bd47e5701672faa
sriharsha.hota <sriharsha.hota@mu-sigma.com>
2018-08-20 17:39:31 +0530

    started all over 3

--- e2e/tests/test.js
+++ e2e/tests/test.js
@@ -7,10 +7,10 @@ describe('React Test', ()=> {
         expect(browser.getTitle()).toEqual('React');
     });
 
-    it('Count of nav options',()=>{
-        browser.sleep(200);
-        var cont = element(by.css('.App h1')).getText();
-        expect(cont).toEqual('Welcome to React');
-    })
+    // it('Count of nav options',()=>{
+    //     browser.sleep(200);
+    //     var cont = element(by.css('.App h1')).getText();
+    //     expect(cont).toEqual('Welcome to React');
+    // })
     
 });
