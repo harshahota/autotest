d3315b114300095c0528f67201eeef6ac0c06997
sriharsha.hota <sriharsha.hota@mu-sigma.com>
2018-08-23 18:32:20 +0530

    test3

--- error.sh
+++ error.sh
@@ -2,7 +2,8 @@
 
 echo "......................................"
 echo ".........the error message............"
+echo ".........the error mail $1............"
 echo "......................................"
 echo "root=harshahota@gmail.com\nmailhub=smtp.gmail.com:587\nAuthUser=jenkinsupdates@gmail.com\nAuthPass=jenkinspassword\nUseTLS=YES\nUseSTARTTLS=YES\nFromLineOverride=YES" >> /etc/ssmtp/ssmtp.conf
 zip -r report.zip target/report
-echo "Test Cases Failed" | mail -s "Fail Report" harshahota123@gmail.com -A report.zip
+echo "Test Cases Failed" | mail -s "Fail Report" $1 -A report.zip
--- pass.sh
+++ pass.sh
@@ -2,7 +2,8 @@
 
 echo "......................................"
 echo ".........the pass message............"
+echo ".........the pass mail $1............"
 echo "......................................"
 echo "root=harshahota@gmail.com\nmailhub=smtp.gmail.com:587\nAuthUser=jenkinsupdates@gmail.com\nAuthPass=jenkinspassword\nUseTLS=YES\nUseSTARTTLS=YES\nFromLineOverride=YES" >> /etc/ssmtp/ssmtp.conf
 zip -r report.zip target/report
-echo "Test cases Passed" | mail -s "Pass Report" harshahota123@gmail.com -A report.zip
+echo "Test cases Passed" | mail -s "Pass Report" $1 -A report.zip
